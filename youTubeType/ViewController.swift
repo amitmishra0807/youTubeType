090f50e81889fceab0d73e559b5a03f6d9b57c8e
Amit <amit.mishra@pristyncare.com>
2020-06-21 22:17:48 +0530

    Initial Commit

---
+++ youTubeType/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youTubeType
+//
+//  Created by Pristyn care on 21/06/20.
+//  Copyright © 2020 Amit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
